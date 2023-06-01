77b4ff7498df8f274fa85562611bcc9a4fb27eb4
OLoKo64 <naldoepingo1@gmail.com>
2023-05-31 22:07:18 -0300

    first commit

---
+++ src/arg_parser.rs
@@ -0,0 +1,33 @@
+use clap::Parser;
+
+/// A simple rename tool
+#[derive(Parser)]
+#[command(version)]
+pub struct AppArguments {
+    /// The pattern to search for
+    pub pattern: String,
+
+    /// Do not rename, just print what would be renamed
+    #[arg(short, long)]
+    pub no_act: bool,
+
+    /// Only rename files, not directories
+    #[arg(short, long, group = "file_or_dir")]
+    pub files_only: bool,
+
+    /// Only rename directories, not files
+    #[arg(short, long, group = "file_or_dir")]
+    pub dirs_only: bool,
+
+    /// The replacement starting range to use
+    #[arg(short, long, default_value_t = 1)]
+    pub start_index: usize,
+
+    /// The replacement file name
+    #[arg(short, long)]
+    pub replacement: Option<String>,
+
+    /// Bypass the confirmation prompt and rename all entries
+    #[arg(short, long)]
+    pub yes: bool,
+}
---
+++ src/main.rs
@@ -0,0 +1,152 @@
+mod arg_parser;
+
+use clap::Parser;
+use glob::{glob, GlobError, PatternError};
+use owo_colors::OwoColorize;
+use std::{io::Write, path::Path};
+
+use crate::arg_parser::AppArguments;
+
+fn main() {
+    let args = AppArguments::parse();
+
+    run(&args).unwrap();
+}
+
+fn run(args: &AppArguments) -> Result<(), Box<dyn std::error::Error>> {
+    let all_entries = all_files_in_dir(&args.pattern)?;
+    if all_entries.is_empty() {
+        println!("No entries found for pattern {}", args.pattern.yellow());
+        return Ok(());
+    }
+
+    if args.files_only {
+        let files = only_files_entries(all_entries);
+        execute(args, &files)?;
+    } else if args.dirs_only {
+        let dirs = only_dirs_entries(all_entries);
+        execute(args, &dirs)?;
+    } else {
+        execute(args, &all_entries)?;
+    }
+
+    Ok(())
+}
+
+fn execute(args: &AppArguments, entries: &[std::path::PathBuf]) -> Result<(), std::io::Error> {
+    if args.no_act {
+        no_act_entries(args, entries, args.start_index);
+    } else {
+        if args.yes || ask_user("Are you sure you want to rename these entries? (y/N): ")? {
+            rename_entries(args, entries, args.start_index)?;
+
+            if !args.no_act {
+                println!("\n{}", "All entries were renamed!".green());
+            }
+        }
+        println!("{}", "Aborting rename".yellow());
+        return Ok(());
+    }
+
+    Ok(())
+}
+
+fn no_act_entries(args: &AppArguments, files: &[std::path::PathBuf], replacement: usize) {
+    for (i, file) in files.iter().enumerate() {
+        println!(
+            "Would rename {} to {}",
+            file.display().green(),
+            new_name(args, file, replacement + i).green()
+        );
+    }
+}
+
+fn new_name(args: &AppArguments, file: &std::path::Path, replacement: usize) -> String {
+    let extension = if file.is_dir() {
+        file.extension()
+            .unwrap_or_default()
+            .to_str()
+            .unwrap_or_default()
+            .to_string()
+    } else {
+        let extension = file
+            .extension()
+            .unwrap_or_default()
+            .to_str()
+            .unwrap_or_default();
+        if extension.is_empty() {
+            String::new()
+        } else {
+            format!(".{extension}")
+        }
+    };
+    let file_path = file.parent().unwrap();
+    format!(
+        "{}-{}{}",
+        file_path
+            .join(
+                args.replacement
+                    .as_deref()
+                    .unwrap_or(Path::new(file).file_stem().unwrap().to_str().unwrap())
+            )
+            .to_str()
+            .unwrap(),
+        replacement,
+        extension
+    )
+}
+
+fn rename_entries(
+    args: &AppArguments,
+    files: &[std::path::PathBuf],
+    replacement: usize,
+) -> Result<(), std::io::Error> {
+    for (i, file) in files.iter().enumerate() {
+        let new_name = new_name(args, file, replacement + i);
+        std::fs::rename(file, &new_name)?;
+        println!("Renamed {} to {}", file.display().green(), new_name.green());
+    }
+
+    Ok(())
+}
+
+fn only_dirs_entries(paths: Vec<std::path::PathBuf>) -> Vec<std::path::PathBuf> {
+    paths.into_iter().filter(|p| p.is_dir()).collect()
+}
+
+fn only_files_entries(paths: Vec<std::path::PathBuf>) -> Vec<std::path::PathBuf> {
+    paths.into_iter().filter(|p| p.is_file()).collect()
+}
+
+fn all_files_in_dir(pattern: &str) -> Result<Vec<std::path::PathBuf>, GlobErrors> {
+    let paths = glob(pattern).map_err(GlobErrors::PatternError)?;
+
+    let path: Result<Vec<_>, _> = paths.collect();
+    path.map_err(GlobErrors::GlobError)
+}
+
+fn ask_user(question: &str) -> Result<bool, std::io::Error> {
+    print!("{question}");
+    std::io::stdout().flush()?;
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input)?;
+
+    Ok(input.to_lowercase().trim() == "y")
+}
+
+#[derive(Debug)]
+enum GlobErrors {
+    PatternError(PatternError),
+    GlobError(GlobError),
+}
+
+impl std::error::Error for GlobErrors {}
+
+impl std::fmt::Display for GlobErrors {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            GlobErrors::PatternError(err) => write!(f, "PatternError: {err}"),
+            GlobErrors::GlobError(err) => write!(f, "GlobError: {err}"),
+        }
+    }
+}
